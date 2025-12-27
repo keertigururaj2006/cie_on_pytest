f589c4691f1c8e6d0f675bca001135b196c6c24b
yashvanttalawar6-creator <yashvanttalawar6@gmail.com>
2025-12-27 12:35:17 +0530

    Saving test file

---
+++ test_student.py
@@ -0,0 +1,12 @@
+
+from student import student_details
+
+def test_student_details():
+    expected_output = (
+        "Student Name: Keerti\n"
+        "USN: 01FE24BCA284\n"
+        "DIVISION: E\n"
+        "Age: 19"
+    )
+
+    assert student_details("Keerti", "01FE24BCA284", "E", 19) == expected_output
